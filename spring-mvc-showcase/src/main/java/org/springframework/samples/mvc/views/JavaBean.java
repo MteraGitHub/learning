89379c993b834493ce41ffae4bdf129601829791
Mtera <test@live.com>
2015-06-02 11:19:37 -0400

    changed Java Bean

--- spring-mvc-showcase/src/main/java/org/springframework/samples/mvc/views/JavaBean.java
+++ spring-mvc-showcase/src/main/java/org/springframework/samples/mvc/views/JavaBean.java
@@ -10,14 +10,14 @@ public class JavaBean {
 	@NotNull
 	private String fruit;
 
-	private String test1;
+	private String test2;
 	
-	public String getTest1() {
-		return test1;
+	public String getTest2() {
+		return test2;
 	}
 
-	public void setTest1(String test1) {
-		this.test1 = test1;
+	public void setTest2(String test2) {
+		this.test2 = test2;
 	}
 
 	public String getFoo() {
